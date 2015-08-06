d2f21714c2d2b55a4b7ba12b23b35e45424b01aa
zhucheng.zc <zhucheng.zc@alibaba-inc.com>
2015-08-06 16:32:14 +0800

    更新测试: 格式整理

--- tests/test-util.js
+++ tests/test-util.js
@@ -4,18 +4,27 @@ var fiCalcs = {
   'bignumber.js': require('../index-v-bignumber.js')
 };
 
+function init(fiCalc) {
+  // 暂时按 optionNew 设置 Money, 执行完 fn 后恢复.
+  fiCalc.Money.optionTmp = function (optionNew, fnTmp) {
+    var optionOld = fiCalc.Money.option();
+    fiCalc.Money.option(optionNew);
+    fnTmp();
+    fiCalc.Money.option(optionOld);
+  };
+}
+
+(function () {
+  for (var key in fiCalcs) {
+    init(fiCalcs[key]);
+  }
+})();
+
 module.exports = {
   // 逐个测试各依赖版本的 fiCalc
   judge: function (fn) {
     for (var key in fiCalcs) {
       (function (key, fiCalc) {
-        // 暂时按 optionNew 设置 Money, 执行完 fn 后恢复.
-        fiCalc.Money.optionTmp = function (optionNew, fnTmp) {
-          var optionOld = fiCalc.Money.option();
-          fiCalc.Money.option(optionNew);
-          fnTmp();
-          fiCalc.Money.option(optionOld);
-        };
         fn(key, fiCalc);
       })(key, fiCalcs[key]);
     }
