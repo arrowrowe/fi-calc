488a91ba7ac70ec4ec22f65ed869b1007526318a
zhucheng.zc <zhucheng.zc@alibaba-inc.com>
2015-08-10 17:07:48 +0800

    更新还款计划: 按月付息支持 onDay

--- libs/repay/repay-i-v-core.js
+++ libs/repay/repay-i-v-core.js
@@ -5,26 +5,36 @@ module.exports = function (Money, ru) {
 
     // 本金总额
     var all = option.all;
-    // 每期利率
-    var rate = option.ratePerPeriod;
     // 期数
     var periodsCount = option.periodsCount;
+
+    // 取整方式
+    var proximateInterest = Money.option('proximateInterest');
 
     // 每期还款本金
     var repayPrincipal = new Money(0);
     // 每期还款利息
-    var repayInterest = all.times(rate).toCent();
+    var repayInterest;
+    if (option.ratePerPeriod !== undefined) {
+      repayInterest = all.times(option.ratePerPeriod).toCent(proximateInterest);
+    }
     // 期
     var periods = [];
 
     // 最后一期单独计算
     for (var n = 1; n < periodsCount; n++) {
+      if (option.onDay) {
+        repayInterest = all.times(option.rates[n - 1]).toCent(proximateInterest);
+      }
       periods.push(ru.getPeriod({
         repayPrincipal: repayPrincipal,
         repayInterest: repayInterest
       }, n));
     }
 
+    if (option.onDay) {
+      repayInterest = all.times(option.rates[n - 1]).toCent(proximateInterest);
+    }
     periods.push(ru.getPeriod({
       repayPrincipal: all,
       repayInterest: repayInterest
